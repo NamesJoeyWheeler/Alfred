a58c75cf993f20cad31ca38adda13d71b17b44e4
NamesJoeyWheeler <36152493+NamesJoeyWheeler@users.noreply.github.com>
2018-02-27 21:04:56 +1100

    Update index.js

--- index.js
+++ index.js
@@ -71,4 +71,10 @@ client.on('message', msg => {
   }
 });
 
+client.on('message', msg => {
+  if (msg.content === 'Other Programs') {
+    msg.reply('Perhaps this might interest you? https://github.com/NamesJoeyWheeler/Millenium-Eye My creator made it.');
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
